a340d1aab651338fc26c95f0fe5e52622dd7361d
cladams0203 <cladams0203@gmail.com>
2020-06-18 12:53:14 -0400

    finished array storage structure for stack

--- stack/stack.py
+++ stack/stack.py
@@ -15,13 +15,14 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
-    def pop(self):
-        pass
+    def pop(self, pos=-1):
+        if len(self.storage) > 0:
+            return self.storage.pop(pos)
